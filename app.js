4926325d1af465818bded84e9c7c7892693cacbc
Carlos <carlosdiones28@gmail.com>
2022-03-11 21:12:50 -0300

    functional but with a few bugs

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const todoInput = document.querySelector('.todo-input');
 const todoBtn = document.querySelector('.todo-btn');
 const todoList = document.querySelector('.todo-list');
 
+const checkElement = '<img class="check-icon" src="icons/check-mark.png">';
+const deleteElement = '<img class="trash-icon" src="icons/trash-can.png">';
+
 //Event Listeners
 todoBtn.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
@@ -15,12 +18,20 @@ function addTodo(event){
                 <span class="todo-text">
                 ${todoInput.value}
                 </span>
-                <img class="check-icon" src="icons/check-mark.png">
-                <img class="trash-icon" src="icons/trash-can.png">
+                ${checkElement}
+                ${deleteElement}
             </li>`;
     todoInput.value = '';
 }
 
-function deleteCheck(event){
-    const item = event.target;
-} // to be continued => Beginner Vanilla JavaScript Project Tutorial 33:00 min
+function deleteCheck(e){
+    const item = e.target;
+    if(item.classList[0] === 'todo-list' ||
+     item.classList[0] === 'todo-container'){
+        return;
+    }
+    if(item){
+        item.remove();
+    }
+} 
+// Fix the bugs and improve the app. ==> Beginner Vanilla JavaScript Project Tutorial 39:00" 
